353653924dd00c2dee3ff41e518b0f568b031056
matthias.staud@gustini.de <matthias.staud@gustini.de>
2021-10-05 22:20:01 +0200

    add connection port (default=3306) to connectionMap and connection dbconnect

--- rentasad.library.db.mysql/src/test/java/rentasad/library/db/MYSQLConnectionTest.java
+++ rentasad.library.db.mysql/src/test/java/rentasad/library/db/MYSQLConnectionTest.java
@@ -8,55 +8,92 @@ import java.util.Map;
 
 import org.junit.jupiter.api.Test;
 
-
 public class MYSQLConnectionTest
 {
 
+	@Test
+	public void testGetParamStringFromConnectionPropertiesMap() throws Exception
+	{
+		Map<String, String> connectionPropertiesMap = MYSQLConnection.getDefaultConnectionPropertiesMap();
 
-    @Test
-    public void testGetParamStringFromConnectionPropertiesMap() throws Exception
-    {
-        Map<String, String> connectionPropertiesMap = MYSQLConnection.getDefaultConnectionPropertiesMap();
-        
-        
-        assertEquals(MYSQLConnection.getParamStringFromConnectionPropertiesMap(connectionPropertiesMap), "useUnicode=true&useJDBCCompliantTimezoneShift=true&allowPublicKeyRetrieval=true&serverTimezone=Europe/Berlin&allowMultiQueries=true&zeroDateTimeBehavior=convertToNull&useLegacyDatetimeCode=false&useSSL=false");
-    }
+		assertEquals(MYSQLConnection.getParamStringFromConnectionPropertiesMap(connectionPropertiesMap),
+				"useUnicode=true&useJDBCCompliantTimezoneShift=true&allowPublicKeyRetrieval=true&serverTimezone=Europe/Berlin&allowMultiQueries=true&zeroDateTimeBehavior=convertToNull&useLegacyDatetimeCode=false&useSSL=false");
+	}
 
-    @Test
-    public void testDbConnectWithTimeZoneUTC() throws Exception
-    {
-        final String MYSQL_HOST = "gstvmdbs3.gustini.local";
-        final String MYSQL_DATABASE = "Endkontrolle_DEV1";
-        final String MYSQL_USER = "greetingcardDev";
-        final String MYSQL_PASSWORD = "greetingcardDev";
-        final String MYSQL_ENCODING = "utf8";
-        HashMap<String, String> mySqlConfigMap = new HashMap<>();
-        mySqlConfigMap.put("MYSQL_HOST", MYSQL_HOST);
-        mySqlConfigMap.put("MYSQL_DATABASE", MYSQL_DATABASE);
-        mySqlConfigMap.put("MYSQL_USER", MYSQL_USER);
-        mySqlConfigMap.put("MYSQL_PASSWORD", MYSQL_PASSWORD);
-        mySqlConfigMap.put("MYSQL_ENCODING", MYSQL_ENCODING);
-        Connection con = MYSQLConnection.dbConnectWithTimeZoneUTC(mySqlConfigMap);
-        con.close();
-    }
-    @Test
-    public void testDbConnectWithMap() throws Exception
-    {
-        final String MYSQL_HOST = "gstvmdbs3.gustini.local";
-        final String MYSQL_DATABASE = "Endkontrolle_DEV1";
-        final String MYSQL_USER = "greetingcardDev";
-        final String MYSQL_PASSWORD = "greetingcardDev";
-        final String MYSQL_ENCODING = "utf8";
-        HashMap<String, String> mySqlConfigMap = new HashMap<>();
-        mySqlConfigMap.put("MYSQL_HOST", MYSQL_HOST);
-        mySqlConfigMap.put("MYSQL_DATABASE", MYSQL_DATABASE);
-        mySqlConfigMap.put("MYSQL_USER", MYSQL_USER);
-        mySqlConfigMap.put("MYSQL_PASSWORD", MYSQL_PASSWORD);
-        mySqlConfigMap.put("MYSQL_ENCODING", MYSQL_ENCODING);
-        Connection con = MYSQLConnection.dbConnect(mySqlConfigMap);
-        con.close();
-    }
-    
-    
+	@Test
+	public void testDbConnectWithTimeZoneUTC() throws Exception
+	{
+		final String MYSQL_HOST = "gstvmdbs3.gustini.local";
+		final String MYSQL_DATABASE = "Endkontrolle_DEV1";
+		final String MYSQL_USER = "greetingcardDev";
+		final String MYSQL_PASSWORD = "greetingcardDev";
+		final String MYSQL_ENCODING = "utf8";
+		HashMap<String, String> mySqlConfigMap = new HashMap<>();
+		mySqlConfigMap.put("MYSQL_HOST", MYSQL_HOST);
+		mySqlConfigMap.put("MYSQL_DATABASE", MYSQL_DATABASE);
+		mySqlConfigMap.put("MYSQL_USER", MYSQL_USER);
+		mySqlConfigMap.put("MYSQL_PASSWORD", MYSQL_PASSWORD);
+		mySqlConfigMap.put("MYSQL_ENCODING", MYSQL_ENCODING);
+		Connection con = MYSQLConnection.dbConnectWithTimeZoneUTC(mySqlConfigMap);
+		con.close();
+	}
+
+	@Test
+	public void testDbConnectWithTimeZoneUTCWithPort3307() throws Exception
+	{
+		final String MYSQL_HOST = "gstvmdbs3.gustini.local";
+		final String MYSQL_DATABASE = "Endkontrolle_DEV1";
+		final String MYSQL_USER = "greetingcardDev";
+		final String MYSQL_PASSWORD = "greetingcardDev";
+		final String MYSQL_ENCODING = "utf8";
+		final String MYSQL_PORT = "3306";
+		HashMap<String, String> mySqlConfigMap = new HashMap<>();
+		mySqlConfigMap.put("MYSQL_HOST", MYSQL_HOST);
+		mySqlConfigMap.put("MYSQL_DATABASE", MYSQL_DATABASE);
+		mySqlConfigMap.put("MYSQL_USER", MYSQL_USER);
+		mySqlConfigMap.put("MYSQL_PASSWORD", MYSQL_PASSWORD);
+		mySqlConfigMap.put("MYSQL_ENCODING", MYSQL_ENCODING);
+		mySqlConfigMap.put("MYSQL_PORT", MYSQL_PORT);
+		Connection con = MYSQLConnection.dbConnectWithTimeZoneUTC(mySqlConfigMap);
+		con.close();
+	}
+
+	@Test
+	public void testDbConnectWithMap() throws Exception
+	{
+		final String MYSQL_HOST = "gstvmdbs3.gustini.local";
+		final String MYSQL_DATABASE = "Endkontrolle_DEV1";
+		final String MYSQL_USER = "greetingcardDev";
+		final String MYSQL_PASSWORD = "greetingcardDev";
+		final String MYSQL_ENCODING = "utf8";
+		HashMap<String, String> mySqlConfigMap = new HashMap<>();
+		mySqlConfigMap.put("MYSQL_HOST", MYSQL_HOST);
+		mySqlConfigMap.put("MYSQL_DATABASE", MYSQL_DATABASE);
+		mySqlConfigMap.put("MYSQL_USER", MYSQL_USER);
+		mySqlConfigMap.put("MYSQL_PASSWORD", MYSQL_PASSWORD);
+		mySqlConfigMap.put("MYSQL_ENCODING", MYSQL_ENCODING);
+		Connection con = MYSQLConnection.dbConnect(mySqlConfigMap);
+		con.close();
+	}
+
+	@Test
+	public void testDbConnectWithMapWithPort() throws Exception
+	{
+		final String MYSQL_HOST = "gstvmdbs3.gustini.local";
+		final String MYSQL_DATABASE = "Endkontrolle_DEV1";
+		final String MYSQL_USER = "greetingcardDev";
+		final String MYSQL_PASSWORD = "greetingcardDev";
+		final String MYSQL_ENCODING = "utf8";
+		final String MYSQL_PORT = "3306";
+		HashMap<String, String> mySqlConfigMap = new HashMap<>();
+		mySqlConfigMap.put("MYSQL_HOST", MYSQL_HOST);
+		mySqlConfigMap.put("MYSQL_DATABASE", MYSQL_DATABASE);
+		mySqlConfigMap.put("MYSQL_USER", MYSQL_USER);
+		mySqlConfigMap.put("MYSQL_PASSWORD", MYSQL_PASSWORD);
+		mySqlConfigMap.put("MYSQL_ENCODING", MYSQL_ENCODING);
+		mySqlConfigMap.put("MYSQL_PORT", MYSQL_PORT);
+		Connection con = MYSQLConnection.dbConnect(mySqlConfigMap);
+		con.close();
+	}
 
 }
