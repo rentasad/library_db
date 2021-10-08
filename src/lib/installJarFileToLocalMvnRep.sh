d7c6d61d043c18a958c32cef6aaa74f89150b42b
wbr <Walter.Brodowsky@gustini.de>
2021-10-08 11:45:15 +0200

    bash script for local jar installation

---
+++ src/lib/installJarFileToLocalMvnRep.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+mvn install:install-file -Dfile=adsjdbc11.0.jar -DgroupId=com.extendedsystems.jdbc.advantage -DartifactId=adsjdbc -Dversion=11.0 -Dpackaging=jar
+mvn install:install-file -Dfile=adsjdbc11.10.jar -DgroupId=com.extendedsystems.jdbc.advantage -DartifactId=adsjdbc -Dversion=11.10 -Dpackaging=jar
+mvn install:install-file -Dfile=adsjdbc12.0.jar -DgroupId=com.extendedsystems.jdbc.advantage -DartifactId=adsjdbc -Dversion=12.0 -Dpackaging=jar
+wait
